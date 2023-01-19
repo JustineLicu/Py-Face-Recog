3253ef6aee56ec20704572470cfc3a9de6c6cafe
Justine Licuanan <caissoone@gmail.com>
2023-01-19 13:19:42 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,124 @@
+import cv2
+import numpy as np
+import face_recognition as fr
+import tkinter as tk
+import customtkinter as ct
+from PIL import Image, ImageTk
+from start_id import sid
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ct.set_appearance_mode("Light")
+
+
+class App(ct.CTk):
+    WIDTH = 800
+    HEIGHT = 600
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("Face Recognition using OpenCV")
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.resizable(width=False, height=False)
+        self.iconbitmap(BASE_DIR + "\\poweredby24.ico")
+
+        self.img_holder = ct.CTkLabel(master=self, text="")
+        self.img_holder.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.cmd_sid = ct.CTkButton(master=self, text="Start ID", command=self.start_id)
+        self.cmd_sid.grid(row=0, column=0, pady=(20, 5), padx=20, sticky="w")
+
+        self.cmd_camera = ct.CTkButton(
+            master=self, text="Open Camera", command=self.open_camera
+        )
+        self.cmd_camera.grid(row=1, column=0, pady=5, padx=20, sticky="w")
+
+        self.cmd_save = ct.CTkButton(master=self, text="Save", command=self.save_img)
+        self.cmd_save.grid(row=2, column=0, pady=5, padx=20, sticky="w")
+
+        self.cmd_retake = ct.CTkButton(master=self, text="Retake", command=self.retake)
+        self.cmd_retake.grid(row=3, column=0, pady=5, padx=20, sticky="w")
+
+        self.cmd_exit = ct.CTkButton(master=self, text="Exit", command=self.exit_app)
+        self.cmd_exit.grid(row=4, column=0, pady=5, padx=20, sticky="w")
+
+        self.cam_active = False
+        self.camerabtn_switch(False)
+        self.btn_switch(self.cmd_save, stat=False)
+        self.btn_switch(self.cmd_retake, stat=False)
+
+    def start_id(self):
+        sid(self)
+
+    def open_camera(self):
+        self.cam_active = True
+        self.camerabtn_switch(True)
+
+        self.cap = cv2.VideoCapture(0)
+
+        success, frame = self.cap.read()
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+
+        self.prevImg = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=self.prevImg)
+        self.img_holder.imgtk = imgtk
+        self.img_holder.configure(image=imgtk)
+
+        # if cv2.waitKey(1) & 0xFF == ord("c"):
+        #     break
+
+    def capture(self):
+        self.cam_active = False
+        self.btn_switch(self.cmd_camera, stat=False)
+        self.btn_switch(self.cmd_save, stat=True)
+        self.btn_switch(self.cmd_retake, stat=True)
+
+        self.txt_imgfname = ct.CTkEntry(
+            master=self, width=240, placeholder_text="e.g.: Steve Rogers"
+        )
+        self.txt_imgfname.grid(row=5, column=0, pady=5, padx=20, sticky="w")
+
+    def save_img(self):
+        success, frame = self.cap.read()
+        cv2.imwrite(
+            BASE_DIR + "\\ImageList\\" + self.txt_imgfname.get() + ".png", frame
+        )
+
+        self.txt_imgfname.destroy()
+        self.camerabtn_switch(False)
+        self.btn_switch(self.cmd_save, stat=False)
+        self.btn_switch(self.cmd_retake, stat=False)
+
+    def retake(self):
+        self.txt_imgfname.destroy()
+        self.btn_switch(self.cmd_camera, stat=True)
+        self.btn_switch(self.cmd_save, stat=False)
+        self.btn_switch(self.cmd_retake, stat=False)
+
+    def exit_app(self):
+        self.quit()
+
+    def camerabtn_switch(self, stat: bool):
+        if not stat:
+            self.cmd_camera = ct.CTkButton(
+                master=self, text="Open Camera", command=self.open_camera
+            )
+            self.cmd_camera.grid(row=1, column=0, pady=5, padx=20, sticky="w")
+        else:
+            self.cmd_camera = ct.CTkButton(
+                master=self, text="Capture", command=self.capture
+            )
+            self.cmd_camera.grid(row=1, column=0, pady=5, padx=20, sticky="w")
+
+    def btn_switch(self, btn: ct.CTkButton, stat: bool):
+        if not stat:
+            btn.configure(state="disabled")
+        else:
+            btn.configure(state="normal")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
